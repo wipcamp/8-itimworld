e16ea6d2c95ed9799d4ff750fae849da60945278
ketaro16 <aphiphit.ketaro@mail.kmutt.ac.th>
2016-01-31 20:01:19 +0700

    Update routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,20 +1,11 @@
 <?php
-
 //Route::get('/upload', 'AdminDocumentController@index');
 //Route::get('/upload/show', 'AdminDocumentController@show');
-
 Route::controller('itim', 'RegisterController');
-
 Route::get('/',function(){
   $password = Hash::make(123);
   $pass = bcrypt(123);
   return "Taro request";
 });
-
 Route::controller('account','AccountController');
-
 Route::controller('dashboard','DashboardController');
-
- 
-// Start Online Page
-
