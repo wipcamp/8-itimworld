8635354ffe4507f525d8e762ee3d4c111684142d
worachatsun <worachat.sun@mail.kmutt.ac.th>
2016-02-11 23:20:18 +0700

    push

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -64,9 +64,9 @@ class AuthController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        return User::create([ 
             'name' => $data['name'],
-            'email' => $data['email'], 
+            'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
     }
