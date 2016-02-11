e22ad929e9414e6d6db39b2e2e1ddb4b7a3842ca
worachatsun <worachat.sun@mail.kmutt.ac.th>
2016-02-11 21:40:20 +0700

    a

---
+++ public/themes/itim_world/views/upload/upload.blade.php
@@ -0,0 +1,76 @@
+<div class="container-fluid">
+         <!-- Scene -->
+         <div id="scene">
+            <div class="layer layout">
+               <div class="town">
+                  <div class="town-front"></div>
+               </div>
+            </div>
+            <div class="layer layout">
+               <div class="town">
+                  <div class="town-mid"></div>
+               </div>
+            </div>
+            <div class="layer layout" style="z-index:-1;">
+               <div class="town">
+                  <div class="town-back"></div>
+               </div>
+            </div>
+         </div>
+         <!-- Header -->
+         <div class="header">
+            <div class="container">
+               <div class="col-xs-2 avatar">
+                  <img class="img-circle img-responsive" src="assets/img/pro3.png">
+               </div>
+               <div class="col-xs-6">
+                  <h1>สวัสดีครับ น้องออฟ</h1>
+                  <h3>WIP ID 12309</h3>
+               </div>
+               <div class="col-xs-4 logout">
+                  <div class="logout-btn">
+                     <button class="logout">ออกจากระบบ</button>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <!--Content-->
+         <div class="content">
+            <div class="container">
+              <br>
+              <br>
+               <div class="col-xs-12 row main-button">
+                  <div class="overlay"></div>
+                  <div class="text row">
+                     <div class="title col-xs-10">
+                        <h1>อัพโหลดใบสมัคร</h1>
+                     </div>
+                     <div class="status col-xs-2">
+                        <h2><i class="fa fa-times"></i></h2>
+                      </div>
+                  </div>
+                  <div class="bg-btn">
+                     <img src="assets/bar/Bar-Sup.png" class="img-responsive" alt="">
+                  </div>
+               </div>
+               <div class="col-xs-12 row main-button">
+                  <div class="overlay"></div>
+                  <div class="text row">
+                     <div class="title col-xs-10">
+                        <h1>อัพโหลดใบ ปพ.1</h1>
+                     </div>
+                     <div class="status col-xs-2">
+                        <h2><i class="fa fa-check"></i></h2>
+                     </div>
+                  </div>
+                  <div class="bg-btn">
+                     <img src="assets/bar/Bar-Cat.png" class="img-responsive" alt="">
+                  </div>
+               </div>
+            </div>
+         </div>
+         <!--End Content-->
+      <div style="position:fixed; width:300px; right:0px; bottom:5px; text-align:right; z-index:50;">
+
+      </div>
+    </div>
