82ba8dc623d898687d2bfcb9e607e4a425b395f0
ketaro16 <aphiphit.ketaro@mail.kmutt.ac.th>
2016-02-12 16:42:11 +0700

    Update routes.php
    logout

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,7 +20,7 @@ Route::group(['middleware' => ['web']], function () {
     Route::controller('auth','Auth\AuthController');
     Route::any('accounts/login/{provider?}','Auth\AuthController@handle_login');
     Route::controller('account','AccountController');
-    Route::controller('auth/logout}','AccountController@getLogout');
+    Route::controller('auth/logout','AccountController@getLogout');
     Route::group(['middleware' => ['web','checkpermission']], function () {
         Route::controller('dashboard','DashboardController');
         Route::get('session',function(){
