208f98182b5f072d59ed9146cc1825280247dc84
trrekiiz <trrekiiz@camphub.in.th>
2016-02-12 18:01:14 +0700

    add dropdown

--- public/themes/itim_world/assets/js/js.js
+++ public/themes/itim_world/assets/js/js.js
@@ -85,9 +85,7 @@ $('.previous').on('click', goToPreviousSlide);
 
 }) ();
 
-//date in bootstrap
-
-
+//
 
 $(document).ready(function() {
   simpleSelect();
@@ -97,18 +95,52 @@ function simpleSelect() {
   "use strict";
   var selectHolder,
     selectClass;
+
   //Setup
-  $('select').each(function() {
-    selectClass = $(this).attr('class');
-    selectHolder = '<dl class="simpleSelect ' + selectClass + '">';
-    selectHolder += '<dt>' + $('option', this).first().text() + '</dt><dd><ul>';
-    $('option', this).each(function() {
-      selectHolder += '<li data="' + $(this).val() + '">' + $(this).text() + '</li>';
-    });
-    selectHolder += '</ul></dd></dl>';
-    $(this).after(selectHolder);
-    $('.' + selectClass).wrapAll('<div class="selectContainer"></div>');
+  // $('select').each(function() {
+  //   selectClass = $(this).attr('class');
+  //   selectHolder = '<dl class="simpleSelect ' + selectClass + '">';
+  //   selectHolder += '<dt>' + $('option', this).first().text() + '</dt><dd><ul>';
+  //   $('option', this).each(function() {
+  //     selectHolder += '<li data="' + $(this).val() + '">' + $(this).text() + '</li>';
+  //   });
+  //   selectHolder += '</ul></dd></dl>';
+  //   $(this).after(selectHolder);
+  //   $('.' + selectClass).wrapAll('<div class="selectContainer"></div>');
+  // });
+// select-sex
+  selectClass = $('#select-sex').attr('class');
+  selectHolder = '<dl class="simpleSelect ' + selectClass + '">';
+  selectHolder += '<dt>' + $('option', '#select-sex').first().text() + '</dt><dd><ul>';
+  $('option', '#select-sex').each(function() {
+    selectHolder += '<li data="' + $('#select-sex').val() + '">' + $('#select-sex').text() + '</li>';
   });
+  selectHolder += '</ul></dd></dl>';
+  $('#select-sex').after(selectHolder);
+  $('.' + selectClass).wrapAll('<div class="selectContainer"></div>');
+
+// select-level
+  selectClass = $('#select-level').attr('class');
+  selectHolder = '<dl class="simpleSelect ' + selectClass + '">';
+  selectHolder += '<dt>' + $('option', '#select-level').first().text() + '</dt><dd><ul>';
+  $('option', '#select-level').each(function() {
+    selectHolder += '<li data="' + $('#select-level').val() + '">' + $('#select-level').text() + '</li>';
+  });
+  selectHolder += '</ul></dd></dl>';
+  $('#select-level').after(selectHolder);
+  $('.' + selectClass).wrapAll('<div class="selectContainer"></div>');
+
+  selectClass = $('#select-program').attr('class');
+  selectHolder = '<dl class="simpleSelect ' + selectClass + '">';
+  selectHolder += '<dt>' + $('option', '#select-program').first().text() + '</dt><dd><ul>';
+  $('option', '#select-program').each(function() {
+    selectHolder += '<li data="' + $('#select-program').val() + '">' + $('#select-program').text() + '</li>';
+  });
+  selectHolder += '</ul></dd></dl>';
+  $('#select-program').after(selectHolder);
+  $('.' + selectClass).wrapAll('<div class="selectContainer"></div>');
+
+
 
   //Clicks
   $('.simpleSelect dd ul li').on("click", function() {
