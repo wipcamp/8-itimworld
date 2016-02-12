01994d072be5f5f45d6acc46cb2459af5e4b6f80
ketaro16 <aphiphit.ketaro@mail.kmutt.ac.th>
2016-02-12 15:04:42 +0700

    Update AuthenticateUser.php
    Session

--- app/Http/Controllers/Auth/AuthenticateUser.php
+++ app/Http/Controllers/Auth/AuthenticateUser.php
@@ -35,6 +35,7 @@ class AuthenticateUser {
             $user = Auth::attempt($user_data);
 //            echo Hash::make(array_get($request, 'password', ''));
 //            dd($user);
+            Session::push('user', '$user');
             if($user){
                 echo "PASS";
             }else{
