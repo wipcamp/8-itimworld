dd0e5195ab614d6f121b410522767ca699c3fa49
worachatsun <worachat.sun@mail.kmutt.ac.th>
2016-02-12 22:35:04 +0700

    cl

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,29 +1,29 @@
 <?php
 Route::controller('itim', 'RegisterController');
-
+Route::get('/',function(){
+  //return redirect('auth/login');
+});
 
 
 Route::get('mail/{wip_id}/{verify}','AccountController@VerifySecond');
-Route::controller('profile','ProfileController');
-Route::controller('question','QuestionController');
 Route::group(['middleware' => ['web']], function () {
     Route::controller('auth','Auth\AuthController');
+    Route::controller('profile','ProfileController');
+    Route::controller('question','QuestionController');
     Route::any('accounts/login/{provider?}','Auth\AuthController@handle_login');
     Route::controller('account','AccountController');
     Route::get('logout','AccountController@getLogout');
     Route::group(['middleware' => ['web','checkpermission']], function () {
         Route::controller('dashboard','DashboardController');
+
         Route::get('session',function(){
           return Session::get('user');
         });
-        Route::get('/',function(){
-          $value = Session::get('user');
-          return array_get($value,'0.0.wip_id');
-        });
-        
-        
+
+
+
     });
-    
-    
+
+
 });
 Route::controller('document','DocumentController');
