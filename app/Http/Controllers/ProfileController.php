aa96829dfaaa10b8a1529ac7ce029ec24744182c
worachatsun <worachat.sun@mail.kmutt.ac.th>
2016-02-12 19:37:53 +0700

    a

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -28,7 +28,7 @@ class ProfileController extends ITIMController{
     public function getRegister(){
         $value = Session::get('user');
         $result = $this->ProfileRepository->find(22222);
-        $data = array_get($value, '0.wip_id');
+        $data = array_get($value, '0.0.wip_id');
         $result = $this->ProfileRepository->find($data);
         $data = json_decode($result,true);
         return $data.'aa';//$this->theme->scope('profile.register',array_get($data,'0'))->layout('blank')->render();
