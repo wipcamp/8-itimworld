d395db05ff9ec89b91acb083a33b6d11bf8843eb
trrekiiz <trrekiiz@camphub.in.th>
2016-02-12 16:40:25 +0700

    add photo upload

--- public/themes/itim_world/assets/js/js.js
+++ public/themes/itim_world/assets/js/js.js
@@ -231,3 +231,27 @@ $(document).ready(function() {
         }
     });
 });
+
+//
+// photo
+
+$(function () {
+
+    "use strict";
+
+    function url(input) {
+        if (input.files && input.files[0]) {
+
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $(".target").attr("src", e.target.result);
+            };
+
+            reader.readAsDataURL(input.files[0]);
+
+        }
+    }
+    $("#file").change(function () {
+        url(this);
+    });
+});
