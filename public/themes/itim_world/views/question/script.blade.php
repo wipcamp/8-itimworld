b7066464539ffffb8d71ecccbef5d851d890c0b7
natahwat_thu <n.thumthiwong@gmail.com>
2016-02-14 00:37:14 +0700

    edit html5 editor toolbar

--- public/themes/itim_world/views/question/script.blade.php
+++ public/themes/itim_world/views/question/script.blade.php
@@ -1,22 +1,26 @@
 {!!HTML::script('themes/itim_world/assets/js/plugins/froala/froala_editor.min.js')!!}
 {!!HTML::script('themes/itim_world/assets/js/plugins/froala/plugins/align.min.js')!!}
-{!!HTML::script('themes/itim_world/assets/js/plugins/froala/plugins/code_beautifier.min.js')!!}
-{!!HTML::script('themes/itim_world/assets/js/plugins/froala/plugins/code_view.min.js')!!}
-{!!HTML::script('themes/itim_world/assets/js/plugins/froala/plugins/draggable.min.js')!!}
+{!!HTML::script('themes/itim_world/assets/js/plugins/froala/plugins/colors.min.js')!!}
+{!!HTML::script('themes/itim_world/assets/js/plugins/froala/plugins/entities.min.js')!!}
+{!!HTML::script('themes/itim_world/assets/js/plugins/froala/plugins/font_size.min.js')!!}
 {!!HTML::script('themes/itim_world/assets/js/plugins/froala/plugins/image.min.js')!!}
-{!!HTML::script('themes/itim_world/assets/js/plugins/froala/plugins/image_manager.min.js')!!}
+{!!HTML::script('themes/itim_world/assets/js/plugins/froala/plugins/line_breaker.min.js')!!}
 {!!HTML::script('themes/itim_world/assets/js/plugins/froala/plugins/link.min.js')!!}
 {!!HTML::script('themes/itim_world/assets/js/plugins/froala/plugins/lists.min.js')!!}
-{!!HTML::script('themes/itim_world/assets/js/plugins/froala/plugins/paragraph_format.min.js')!!}
-{!!HTML::script('themes/itim_world/assets/js/plugins/froala/plugins/paragraph_style.min.js')!!}
+{!!HTML::script('themes/itim_world/assets/js/plugins/froala/plugins/quick_insert.min.js')!!}
+{!!HTML::script('themes/itim_world/assets/js/plugins/froala/plugins/quote.min.js')!!}
+{!!HTML::script('themes/itim_world/assets/js/plugins/froala/plugins/save.min.js')!!}
 {!!HTML::script('themes/itim_world/assets/js/plugins/froala/plugins/table.min.js')!!}
-{!!HTML::script('themes/itim_world/assets/js/plugins/froala/plugins/video.min.js')!!}
 {!!HTML::script('themes/itim_world/assets/js/plugins/froala/plugins/url.min.js')!!}
-{!!HTML::script('themes/itim_world/assets/js/plugins/froala/plugins/entities.min.js')!!}
-
+{!!HTML::script('themes/itim_world/assets/js/plugins/froala/plugins/video.min.js')!!}
 
 <script>
     $(function(){
-      $(".answer-box").froalaEditor();
+        $(".answer-box").froalaEditor({
+            height: 300,
+            toolbarSticky: false,
+            codeBeautifier: false,
+            imageInsertButtons: ['imageBack', '|', 'imageByURL'],
+        });
     });
 </script>
