1525e9015a6ff036ad35815c3516272fc5888470
trrekiiz <trrekiiz@camphub.in.th>
2016-02-14 15:09:41 +0700

    add vendor

---
+++ public/themes/itim_world/views/question/q1.blade.php
@@ -0,0 +1,51 @@
+{!!HTML::style('themes/itim_world/assets/css/bootstrap-responsive.css')!!}
+{!!HTML::script('themes/itim_world/assets/js/bootstrap.js')!!}
+      <div class="container-fluid">
+        <!-- Scene -->
+        <div id="scene">
+          <div class="layer layout">
+            <div class="town">
+              <div class="town-front"></div>
+            </div>
+          </div>
+          <div class="layer layout">
+            <div class="town">
+              <div class="town-mid"></div>
+            </div>
+          </div>
+          <div class="layer layout" style="z-index:-1;">
+            <div class="town">
+              <div class="town-back"></div>
+            </div>
+          </div>
+        </div>
+        <!-- Header -->
+        <div class="header">
+          <div class="container">
+            <div class="col-xs-2 avatar">
+              <img class="img-circle img-responsive" src="assets/img/pro3.png">
+            </div>
+            <div class="col-xs-6">
+              <h1>สวัสดีครับ น้องออฟ</h1>
+              <h3>WIP ID 12309</h3>
+            </div>
+            <div class="col-xs-4 logout">
+              <div class="logout-btn">
+                <button type="button" class="logout">ออกจากระบบ</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--Content-->
+        <div class="content">
+          <div class="content">
+           <div class="container">
+               <div class="row" style="margin-top:30px;">
+                   <div class="col-sm-10 col-sm-offset-1">
+                     
+                   </div>
+               </div>
+           </div>
+       </div>
+        </div>
+</div>
