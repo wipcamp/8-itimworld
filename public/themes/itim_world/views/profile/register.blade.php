30317b0358b8c1b5db37cc91395b3721ffcab3a4
trrekiiz <trrekiiz@camphub.in.th>
2016-02-14 12:22:17 +0700

    delete

--- public/themes/itim_world/views/profile/register.blade.php
+++ public/themes/itim_world/views/profile/register.blade.php
@@ -49,25 +49,6 @@
             </div>
           </div>
         </div>
-        <div class="col-xs-3"> <!-- required for floating -->
-    <!-- Nav tabs -->
-            <ul class="nav nav-tabs tabs-left">
-              <li class="active"><a href="#home" data-toggle="tab">Home</a></li>
-              <li><a href="#profile" data-toggle="tab">Profile</a></li>
-              <li><a href="#messages" data-toggle="tab">Messages</a></li>
-              <li><a href="#settings" data-toggle="tab">Settings</a></li>
-            </ul>
-        </div>
-
-        <div class="col-xs-9">
-            <!-- Tab panes -->
-            <div class="tab-content">
-              <div class="tab-pane active" id="home">Home Tab.</div>
-              <div class="tab-pane" id="profile">Profile Tab.</div>
-              <div class="tab-pane" id="messages">Messages Tab.</div>
-              <div class="tab-pane" id="settings">Settings Tab.</div>
-            </div>
-        </div>
         <div class="row">
           <ul class="slide-container container">
             <li data-tag="ข้อตกลงและเงื่อนไข" class="slide">
