c88d13a135cb4677257563edc520021a49a7117d
trrekiiz <trrekiiz@camphub.in.th>
2016-02-14 20:17:35 +0700

    add verify / not verify

--- public/themes/itim_world/views/account/not_verify.blade.php
+++ public/themes/itim_world/views/account/not_verify.blade.php
@@ -1,1 +1,46 @@
-ยังไม่ได้ Verify mail
+<div class="container-fluid">
+            <div id="scene">
+                <div class="layer layout">
+                    <div class="town">
+                        <div class="town-front"></div>
+                    </div>
+                </div>
+                <div class="layer layout">
+                    <div class="town">
+                        <div class="town-mid"></div>
+                    </div>
+                </div>
+                <div class="layer layout" style="z-index:-3;">
+                    <div class="town">
+                        <div class="town-back"></div>
+                    </div>
+                </div>
+            <center>
+                <div class="logo-wip">
+                   <div class="row">
+                       <img src="<?php echo url(""); ?>/themes/itim_world/assets/img/layout/finalwip.png" alt="" width="40%">
+                   </div>
+                   <div class="row">
+                       <img src="<?php echo url(""); ?>/themes/itim_world/assets/img/layout/finalway.png" alt="" width="30%" style="margin-top:-4%">
+                   </div>
+                </div>
+            </center>
+
+        <!--Content-->
+            <div class="row">
+               <center>
+                   <h2>น้องยังไม่ได้ยืนยันอีเมลล์นะครับผม</h2>
+                   <h2>น้องควรเช็คอีเมลล์ขอน้องเพื่อยืนยันตัวตนได้เลยครับ ถ้าไม่เจอดูใน Junk Box น้าครับ</h2>
+                  <br>
+                  <br>
+                  <div class="col-sm-4 col-sm-offset-4">
+                    <a href="<?php echo url(""); ?>/auth/login"><input type="submit" value="เข้าสู่ระบบ"></a>
+                  </div>
+               </center>
+            </div>
+        <!--End Content-->
+            </div>
+            <div style="position:fixed; width:300px; right:0px; bottom:5px; text-align:right; z-index:50;">
+                Chat
+            </div>
+        </div>
